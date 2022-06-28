60246626af8c3a7be8a4e25e8930eb2dd792fb0b
Anishishi <aki.nishi.work@gmail.com>
2022-06-28 17:57:29 +0900

    fix The plugin internet_speed_test uses a deprecated version of the Android embedding

--- android/src/main/kotlin/com/tahamalas/internet_speed_test/InternetSpeedTestPlugin.kt
+++ android/src/main/kotlin/com/tahamalas/internet_speed_test/InternetSpeedTestPlugin.kt
@@ -10,19 +10,23 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 
 
 /** InternetSpeedTestPlugin */
-public class InternetSpeedTestPlugin(internal var activity: Activity, internal var methodChannel: MethodChannel, registrar: Registrar) : MethodCallHandler {
+public class InternetSpeedTestPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
 
-
-    private var result: Result? = null
     private var speedTestSocket: SpeedTestSocket = SpeedTestSocket()
 
-
-    init {
-        this.methodChannel.setMethodCallHandler(this)
+    private lateinit var channel: MethodChannel
+    private var activity: Activity? = null
+    private var applicationContext: Context? = null
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        applicationContext = flutterPluginBinding.applicationContext
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "internet_speed_test")
+        channel.setMethodCallHandler(this)
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
@@ -34,20 +38,34 @@ public class InternetSpeedTestPlugin(internal var activity: Activity, internal v
         }
     }
 
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        activity = null
+        applicationContext = null
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
     private fun mapToCall(result: Result, arguments: Any?) {
         val argsMap = arguments as Map<*, *>
 
         when (val args = argsMap["id"] as Int) {
             CallbacksEnum.START_DOWNLOAD_TESTING.ordinal -> startListening(args, result, "startDownloadTesting", argsMap["testServer"] as String)
             CallbacksEnum.START_UPLOAD_TESTING.ordinal -> startListening(args, result, "startUploadTesting", argsMap["testServer"] as String)
-        }
-    }
-
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "internet_speed_test")
-            channel.setMethodCallHandler(InternetSpeedTestPlugin(registrar.activity(), channel, registrar))
         }
     }
 
